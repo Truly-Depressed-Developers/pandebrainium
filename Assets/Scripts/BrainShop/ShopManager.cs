da45fd1395a9b79b66e0afbfe59c42b4f7e26a89
MBrosik <michal.bros1@gmail.com>
2023-02-25 22:21:05 +0100

    DrawBrainsInScreen

--- Assets/Scripts/BrainShop/ShopManager.cs
+++ Assets/Scripts/BrainShop/ShopManager.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ShopManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   [SerializeField] GameObject brainDataContainerPrefab;
+   [SerializeField] Transform brainRowContainer;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   List<DrawBrain.BrainStatistics> brainStatisticsList = new List<DrawBrain.BrainStatistics>();
+
+   // Start is called before the first frame update
+   void Start()
+   {
+      for (int i = 0; i < 3; i++)
+      {
+         var newBrainStatisticData = DrawBrain.DrawRandomBrain();
+         brainStatisticsList.Add(newBrainStatisticData);
+      }
+
+      foreach (var brainStatistic in brainStatisticsList)
+      {
+         var brainDataGameObject = Instantiate(brainDataContainerPrefab, brainRowContainer);
+
+         var dexterityText = brainDataGameObject.transform.Find("BrainStatsValues/DexterityScore").GetComponent<TMP_Text>();
+         var intelligenceText = brainDataGameObject.transform.Find("BrainStatsValues/IntelligenceScore").GetComponent<TMP_Text>();
+         var strengthText = brainDataGameObject.transform.Find("BrainStatsValues/StrengthScore").GetComponent<TMP_Text>();
+         var costText = brainDataGameObject.transform.Find("Cost").GetComponent<TMP_Text>();
+
+         dexterityText.SetText(brainStatistic.dexterity.ToString());
+         intelligenceText.SetText(brainStatistic.intelligence.ToString());
+         strengthText.SetText(brainStatistic.strength.ToString());
+         costText.SetText(brainStatistic.cost.ToString()+ "$");
+      }
+   }
 }
