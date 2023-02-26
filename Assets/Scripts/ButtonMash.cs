48f9b67e84bf34b22ca29b5ddbde8054f6676c43
MBrosik <michal.bros1@gmail.com>
2023-02-26 02:33:43 +0100

    Added difficulty mod

--- Assets/Scripts/ButtonMash.cs
+++ Assets/Scripts/ButtonMash.cs
@@ -4,32 +4,36 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
 
-public class ButtonMash : MonoBehaviour {
-    [SerializeField] private TMP_Text buttonText;
+public class ButtonMash : MonoBehaviour
+{
+   [SerializeField] private TMP_Text buttonText;
 
-    [SerializeField] private float multiplayer = 2.0f;
-    [SerializeField] private int baseClickCount = 5;
-    private int clickCounter;
+   [SerializeField] private float multiplayer = 2.0f;
+   [SerializeField] private int baseClickCount = 5;
+   private int clickCounter;
 
-    [SerializeField] private ChallengeTrigger challengeScript;
-
-
-    // Start is called before the first frame update
-    void Start() {
-        clickCounter = Mathf.FloorToInt(baseClickCount * multiplayer);
-        buttonText.SetText(clickCounter.ToString());
-    }
+   [SerializeField] private ChallengeTrigger challengeScript;
 
 
-    public void ButtonActivation() {
-        --clickCounter;
+   // Start is called before the first frame update
+   void Start()
+   {
+      clickCounter = Mathf.FloorToInt(baseClickCount * multiplayer * challengeScript.difficultyMod);
+      buttonText.SetText(clickCounter.ToString());
+   }
 
-        if (clickCounter <= 0) {
-            challengeScript.Fulfill();
-        }
 
-        buttonText.SetText(clickCounter.ToString());
-    }
+   public void ButtonActivation()
+   {
+      --clickCounter;
+
+      if (clickCounter <= 0)
+      {
+         challengeScript.Fulfill();
+      }
+
+      buttonText.SetText(clickCounter.ToString());
+   }
 
 
 }
