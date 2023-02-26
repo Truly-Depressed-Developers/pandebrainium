4229670c6c85ab6740975db5a1f3bd554501040b
Spookyless <spookyless.info@gmail.com>
2023-02-26 06:09:09 +0100

    Fixed Osu breaking when total diff < 1.0

--- Assets/Scripts/ConnectingDotsTask/OsuTaskManager.cs
+++ Assets/Scripts/ConnectingDotsTask/OsuTaskManager.cs
@@ -9,83 +9,69 @@ using UnityEngine.Events;
 
 
 
-public class OsuTaskManager : MonoBehaviour
-{
+public class OsuTaskManager : MonoBehaviour {
 
 
-   [SerializeField] GameObject osuPanelContainer;
+    [SerializeField] GameObject osuPanelContainer;
 
-   [SerializeField] GameObject dotPrefab;
+    [SerializeField] GameObject dotPrefab;
 
-   [SerializeField] ChallengeTrigger challengeTrigger;
-
-
-   int pointCount = 5;
-   int currentPointNumber = 1;
+    [SerializeField] ChallengeTrigger challengeTrigger;
 
 
-   // Start is called before the first frame update
-   void Start()
-   {
-      Activate();
-   }
+    int basePoints = 6;
+    int currentPointNumber = 1;
+    int totalPoints;
 
-   // Update is called once per frame
-   void Update()
-   {
+    // Start is called before the first frame update
+    void Start() {
+        Activate();
+    }
 
-   }
+    public void Activate() {
 
-   public void Activate()
-   {
+        var osuPanelTransform = osuPanelContainer.transform;
 
-      var osuPanelTransform = osuPanelContainer.transform;
+        var osuPanelRectTransform = osuPanelTransform.GetComponent<RectTransform>();
 
-      var osuPanelRectTransform = osuPanelTransform.GetComponent<RectTransform>();
+        Vector2 maxSpawnRange = osuPanelRectTransform.sizeDelta;
 
-      Vector2 maxSpawnRange = osuPanelRectTransform.sizeDelta;
+        Vector2 spawnRange = maxSpawnRange - dotPrefab.GetComponent<RectTransform>().sizeDelta;
 
-      Vector2 spawnRange = maxSpawnRange - dotPrefab.GetComponent<RectTransform>().sizeDelta;
+        totalPoints = (int)Mathf.Ceil(basePoints * challengeTrigger.difficultyMod);
 
-      for (int i = (int)Mathf.Floor(pointCount * challengeTrigger.difficultyMod); i >= 1; i--)
-      {
+        for (int i = totalPoints; i >= 1; i--) {
 
-         int randomX = (int)(Random.Range(0, spawnRange.x) - spawnRange.x / 2);
-         int randomY = (int)(Random.Range(0, spawnRange.y) - spawnRange.y / 2);
+            int randomX = (int)(Random.Range(0, spawnRange.x) - spawnRange.x / 2);
+            int randomY = (int)(Random.Range(0, spawnRange.y) - spawnRange.y / 2);
 
-         var osuDotGameObject = Instantiate(dotPrefab, osuPanelTransform);
-         var osuDotRectTransform = osuDotGameObject.GetComponent<RectTransform>();
-         osuDotRectTransform.localPosition = new Vector3(randomX, randomY, osuPanelRectTransform.localPosition.z);
+            var osuDotGameObject = Instantiate(dotPrefab, osuPanelTransform);
+            var osuDotRectTransform = osuDotGameObject.GetComponent<RectTransform>();
+            osuDotRectTransform.localPosition = new Vector3(randomX, randomY, osuPanelRectTransform.localPosition.z);
 
 
-         var textGameObject = osuDotGameObject.transform.GetChild(0);
-         var textComp = textGameObject.GetComponent<TMP_Text>();
+            var textGameObject = osuDotGameObject.transform.GetChild(0);
+            var textComp = textGameObject.GetComponent<TMP_Text>();
 
-         textComp.SetText(i.ToString());
+            textComp.SetText(i.ToString());
 
-         var osuDotClickScript = osuDotGameObject.GetComponent<DotClick>();
+            var osuDotClickScript = osuDotGameObject.GetComponent<DotClick>();
 
-         osuDotClickScript.onClickEvent.AddListener(buttonGetClickInfo);
-      }
+            osuDotClickScript.onClickEvent.AddListener(buttonGetClickInfo);
+        }
 
 
-   }
+    }
 
-   void buttonGetClickInfo(int number)
-   {
-      if (number != currentPointNumber)
-      {
-         challengeTrigger.Fail();
-         currentPointNumber = 1;
-      }
-      else if (number == pointCount)
-      {
-         challengeTrigger.Fulfill();
-         currentPointNumber = 1;
-      }
-      else
-      {
-         currentPointNumber++;
-      }
-   }
+    void buttonGetClickInfo(int number) {
+        if (number != currentPointNumber) {
+            challengeTrigger.Fail();
+            currentPointNumber = 1;
+        } else if (number == basePoints) {
+            challengeTrigger.Fulfill();
+            currentPointNumber = 1;
+        } else {
+            currentPointNumber++;
+        }
+    }
 }
